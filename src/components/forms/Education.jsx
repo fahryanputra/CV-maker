6f11e6fa3187c29b1c1a417152afb1b1ac164f86
fahryanputra <fahryanputra@gmail.com>
2024-05-16 10:47:31 +0700

    Add collapsing education and experience forms

--- src/components/forms/Education.jsx
+++ src/components/forms/Education.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import InputGroup from "../InputGroup";
 import { v4 as uuidv4 } from "uuid";
 
-function EducationForm(data, setData, education) {
+function EducationForm(data, setData, education, index, isVisible, onVisible) {
   function handleEducationChange(id, inputName, e) {
     setData({
       ...data,
@@ -13,47 +14,56 @@ function EducationForm(data, setData, education) {
   }
 
   return (
-    <div key={education.id}>
-      <InputGroup
-        id="school"
-        labelText="School"
-        type="text"
-        placeholder="School or university"
-        onChange={(e) => handleEducationChange(education.id, "school", e)}
-        value={education.school}
-      />
-      <InputGroup
-        id="degree"
-        labelText="Degree"
-        type="text"
-        placeholder="Degree or field of study"
-        onChange={(e) => handleEducationChange(education.id, "degree", e)}
-        value={education.degree}
-      />
-      <InputGroup
-        id="education-start-date"
-        labelText="Start Year"
-        type="text"
-        placeholder="Start year"
-        onChange={(e) => handleEducationChange(education.id, "startDate", e)}
-        value={education.startDate}
-      />
-      <InputGroup
-        id="education-end-date"
-        labelText="End Year"
-        type="text"
-        placeholder='End year or "Present"'
-        onChange={(e) => handleEducationChange(education.id, "endDate", e)}
-        value={education.endDate}
-      />
-      <InputGroup
-        id="education-location"
-        labelText="Location"
-        type="text"
-        placeholder="School's location"
-        onChange={(e) => handleEducationChange(education.id, "location", e)}
-        value={education.location}
-      />
+    <div onClick={onVisible} key={education.id}>
+      {isVisible ? (
+        <div>
+          <h4>Education {index + 1}</h4>
+          <InputGroup
+            id={"school-" + index}
+            labelText="School"
+            type="text"
+            placeholder="School or university"
+            onChange={(e) => handleEducationChange(education.id, "school", e)}
+            value={education.school}
+          />
+          <InputGroup
+            id={"degree-" + index}
+            labelText="Degree"
+            type="text"
+            placeholder="Degree or field of study"
+            onChange={(e) => handleEducationChange(education.id, "degree", e)}
+            value={education.degree}
+          />
+          <InputGroup
+            id={"education-start-date-" + index}
+            labelText="Start Year"
+            type="text"
+            placeholder="Start year"
+            onChange={(e) =>
+              handleEducationChange(education.id, "startDate", e)
+            }
+            value={education.startDate}
+          />
+          <InputGroup
+            id={"education-end-date-" + index}
+            labelText="End Year"
+            type="text"
+            placeholder='End year or "Present"'
+            onChange={(e) => handleEducationChange(education.id, "endDate", e)}
+            value={education.endDate}
+          />
+          <InputGroup
+            id={"education-location-" + index}
+            labelText="Location"
+            type="text"
+            placeholder="School's location"
+            onChange={(e) => handleEducationChange(education.id, "location", e)}
+            value={education.location}
+          />
+        </div>
+      ) : (
+        <h4>{education.school ? education.school : "Blank School"}</h4>
+      )}
     </div>
   );
 }
@@ -75,14 +85,24 @@ function Education({ data, setData, isActive, onShow }) {
         },
       ],
     });
+    setActiveIndex(data.educations.length);
   }
+
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <form className="education">
       <h2 onClick={onShow}>Education</h2>
       {isActive &&
-        data.educations.map((education) =>
-          EducationForm(data, setData, education)
+        data.educations.map((education, index) =>
+          EducationForm(
+            data,
+            setData,
+            education,
+            index,
+            activeIndex === index,
+            () => setActiveIndex(index)
+          )
         )}
       {isActive && data.educations.length < 3 && (
         <button onClick={(e) => addEducation(data, setData, e)}>
--- src/components/forms/Experience.jsx
+++ src/components/forms/Experience.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import InputGroup from "../InputGroup";
 import { v4 as uuidv4 } from "uuid";
 
-function ExperienceForm(data, setData, experience) {
+function ExperienceForm(
+  data,
+  setData,
+  experience,
+  index,
+  isVisible,
+  onVisible
+) {
   function handleExperienceChange(id, inputName, e) {
     setData({
       ...data,
@@ -13,61 +21,76 @@ function ExperienceForm(data, setData, experience) {
   }
 
   return (
-    <div key={experience.id}>
-      <InputGroup
-        id="company-name"
-        labelText="Company Name"
-        type="text"
-        placeholder="Company name"
-        onChange={(e) => {
-          handleExperienceChange(experience.id, "companyName", e);
-        }}
-        value={experience.companyName}
-      />
-      <InputGroup
-        id="position-title"
-        labelText="Position Title"
-        type="text"
-        placeholder="Position title"
-        onChange={(e) =>
-          handleExperienceChange(experience.id, "positionTitle", e)
-        }
-        value={experience.positionTitle}
-      />
-      <InputGroup
-        id="description"
-        labelText="Description"
-        type="textarea"
-        placeholder="Job description"
-        onChange={(e) =>
-          handleExperienceChange(experience.id, "description", e)
-        }
-        value={experience.description}
-      />
-      <InputGroup
-        id="experience-start-date"
-        labelText="Start Year"
-        type="text"
-        placeholder="Start year"
-        onChange={(e) => handleExperienceChange(experience.id, "startDate", e)}
-        value={experience.startDate}
-      />
-      <InputGroup
-        id="experience-end-date"
-        labelText="End Year"
-        type="text"
-        placeholder='End year or "Present"'
-        onChange={(e) => handleExperienceChange(experience.id, "endDate", e)}
-        value={experience.endDate}
-      />
-      <InputGroup
-        id="experience-location"
-        labelText="Company's Location"
-        type="text"
-        placeholder="Company's location"
-        onChange={(e) => handleExperienceChange(experience.id, "location", e)}
-        value={experience.location}
-      />
+    <div onClick={onVisible} key={experience.id}>
+      {isVisible ? (
+        <div>
+          <h4>Experience {index + 1}</h4>
+          <InputGroup
+            id={"company-name-" + index}
+            labelText="Company Name"
+            type="text"
+            placeholder="Company name"
+            onChange={(e) => {
+              handleExperienceChange(experience.id, "companyName", e);
+            }}
+            value={experience.companyName}
+          />
+          <InputGroup
+            id={"position-title-" + index}
+            labelText="Position Title"
+            type="text"
+            placeholder="Position title"
+            onChange={(e) =>
+              handleExperienceChange(experience.id, "positionTitle", e)
+            }
+            value={experience.positionTitle}
+          />
+          <InputGroup
+            id={"description-" + index}
+            labelText="Description"
+            type="textarea"
+            placeholder="Job description"
+            onChange={(e) =>
+              handleExperienceChange(experience.id, "description", e)
+            }
+            value={experience.description}
+          />
+          <InputGroup
+            id={"experience-start-date-" + index}
+            labelText="Start Year"
+            type="text"
+            placeholder="Start year"
+            onChange={(e) =>
+              handleExperienceChange(experience.id, "startDate", e)
+            }
+            value={experience.startDate}
+          />
+          <InputGroup
+            id={"experience-end-date-" + index}
+            labelText="End Year"
+            type="text"
+            placeholder='End year or "Present"'
+            onChange={(e) =>
+              handleExperienceChange(experience.id, "endDate", e)
+            }
+            value={experience.endDate}
+          />
+          <InputGroup
+            id={"experience-location-" + index}
+            labelText="Company's Location"
+            type="text"
+            placeholder="Company's location"
+            onChange={(e) =>
+              handleExperienceChange(experience.id, "location", e)
+            }
+            value={experience.location}
+          />
+        </div>
+      ) : (
+        <h4>
+          {experience.companyName ? experience.companyName : "Blank Company"}
+        </h4>
+      )}
     </div>
   );
 }
@@ -90,14 +113,24 @@ function Experience({ data, setData, isActive, onShow }) {
         },
       ],
     });
+    setActiveIndex(data.experiences.length);
   }
+
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <form className="experience">
       <h2 onClick={onShow}>Experience</h2>
       {isActive &&
-        data.experiences.map((experience) =>
-          ExperienceForm(data, setData, experience)
+        data.experiences.map((experience, index) =>
+          ExperienceForm(
+            data,
+            setData,
+            experience,
+            index,
+            activeIndex === index,
+            () => setActiveIndex(index)
+          )
         )}
       {isActive && data.experiences.length < 3 && (
         <button onClick={(e) => addExperience(data, setData, e)}>
